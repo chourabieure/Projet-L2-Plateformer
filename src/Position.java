d120a68ebc391bccb5d4fae77cd43880fbb0b3f5
Mathis <mathis.antelme@gmail.com>
2018-01-28 12:05:34 +0100

    Create Position.java

---
+++ src/Position.java
@@ -0,0 +1,56 @@
+/**
+ * Position
+ * the position of an element
+ */
+public class Position {
+    // Atributes
+    private double x, y;
+
+    Position (double p_x, double p_y) {
+        this.x = p_x;
+        this.y = p_y;
+    }
+
+
+    // Accesseurs
+    /**
+     * @return the x
+     */
+    public double getX () {
+        return x;
+    }
+
+    /**
+     * @return the y
+     */
+    public double getY () {
+        return y;
+    }
+
+    /**
+     * @param p_x the x to set
+     */
+    public void setX (double p_x) {
+        this.x = p_x;
+    }
+
+    /**
+     * @param p_y the y to set
+     */
+    public void setY (double p_y) {
+        this.y = p_y;
+    }
+
+    // Methods
+
+    /**
+     * moves the point to the p_x p_y location
+     * @param p_x the x location
+     * @param p_y the y location
+     */
+    public Position move (double p_x, double p_y) {
+        this.x = p_x;
+        this.y = p_y;
+        return this; 
+    }
+}
