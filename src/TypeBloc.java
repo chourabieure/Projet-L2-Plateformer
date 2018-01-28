7ce0a5c27c8cf4cad46699ce3f1656685fd79083
Mathis <mathis.antelme@gmail.com>
2018-01-28 12:24:09 +0100

    Creation of TypeBloc.java
    A enumeration of the differents type of blocs present in the game. Used in the graphical interface

---
+++ src/TypeBloc.java
@@ -0,0 +1,21 @@
+/**
+ * TypeBloc
+ * an enumeration of the blocs used in the graphical interface
+ */
+public enum TypeBloc {
+    GRASS (1),
+    STONE (2),
+    BRICKS (3);
+
+    private int type;
+
+    // Constructor
+    TypeBloc (int p_type) {
+        this.type = p_type;
+    }
+
+    @Override
+    public String toString() {
+        return this.type;
+    }
+}
